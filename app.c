b4cdf7eaf1da49d027638285e0bce5caf2b19f4a
xdevelnet <xdevelnet@users.noreply.github.com>
2021-10-13 17:34:30 +0300

    Add pre- and post- app loop actions. Add fool-protection for headers setting
    These protections are:
    
    1. If headers were not sent (SET_HTTP_STATUS_AND_HDR() were not called) then
       first APP_WRITE() call causes automatic SET_HTTP_STATUS_AND_HDR(200, NULL)
       Another subsequent APP_WRITE() calls will be just regular, and
       SET_HTTP_STATUS_AND_HDR() will cause nothing.
    2. If SET_HTTP_STATUS_AND_HDR() were called first, then any further calls to
       SET_HTTP_STATUS_AND_HDR() will cause nothing. Also, APP_WRITE() will become
       regular immediately.
    
    Also, these protections were made not only for protect app from missuse, but
    for keeping consistency with requirements that were made with mongoose integration
    and implementation. In other words, you are not able to use chunked http encoding
    until you delcared it on headers, therefore NOT using SET_HTTP_STATUS_AND_HDR()
    will cause just invalid data output, which is just bad.
    
    These changes were made not with dumb if() checking, but with changing function
    pointer values, which is the best way to deal with such situations.

--- app.c
+++ app.c
@@ -1,26 +1,37 @@
 typedef struct appargs {
 	const char *request;
 	size_t request_len;
+	void *appcontext;
 	void *context1;
 	void *context2;
 } appargs;
 
 const char *(*locate_header)(const char *, size_t *, void *);
 void (*set_http_status_and_hdr) (unsigned short, const char **, void *);
-bool (*app_write) (const void *, unsigned long, void *);
+void (*app_write) (const void *, unsigned long, void *);
 
 #define REQUEST a.request
 #define REQUEST_LEN a.request_len
+#define CONTEXT a.appcontext
 #define LOCATE_HEADER(arg1, arg2) locate_header(arg1, arg2, a.context2)
 #define SET_HTTP_STATUS_AND_HDR(arg1, arg2) set_http_status_and_hdr(arg1, arg2, a.context1)
 #define APP_WRITE(arg1, arg2) app_write(arg1, arg2, a.context1)
 
 const char *table[] = {"Content-Type: text/html;charset=utf-8", "Server: OLOLOLO TROLOLO", NULL};
 
-void app(appargs a) {
+void app_prepare(void **ptr) {
+//	*ptr = malloc(1000);
+}
+
+void app_finish(void *ptr) {
+//	free(ptr);
+}
+
+void app_request(appargs a) {
 	SET_HTTP_STATUS_AND_HDR(200, table);
 	APP_WRITE("Your request is: ", 17);
 	APP_WRITE(REQUEST, REQUEST_LEN);
+	APP_WRITE("\n", 1);
 	APP_WRITE("your user agent: ", 17);
 	size_t s;
 	const char *ua = LOCATE_HEADER("User-Agent", &s);
