4ed312506747baf3871ea54a73905ab1bb654f46
xdevelnet <xdevelnet@users.noreply.github.com>
2021-10-21 20:31:03 +0300

    Add defaults data to separate file

---
+++ default_rodata.h
@@ -0,0 +1,18 @@
+#ifndef CBLOG_DEFAULT_RODATA_H
+#define CBLOG_DEFAULT_RODATA_H
+
+const char default_appname[] = "Blog demo";
+size_t default_appnamelen = strizeof(default_appname);
+source_type default_template_type = SOURCE_FILE;
+const char default_template_name[] = "static/minimalist/index (copy).ssb";
+enum datalayer_engines default_datalayer_type = ENGINE_FILENO;
+const char default_datalayer_addr[] = "demo_data";
+const char default_title_page_name[] = "Welcome to my blog!";
+size_t default_title_page_len = strizeof(default_title_page_name);
+const char default_title_content[] = ""
+	"<h2>Welcome to my personal blog!<h2><br>"
+	"Here you may find some notes and records that I am write from time to time.<br>"
+	"Recent posts:";
+size_t default_title_content_len = strizeof(default_title_content);
+
+#endif //CBLOG_DEFAULT_RODATA_H
